51eaa0f063f9150220843dffd585be3f96ae09c6
firecodes <384502958@qq.com>
2023-12-02 06:11:12 +0800

    增加gulp打包

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,35 @@
 const gulp = require('gulp')
 const startServer = require('./server.js')
-// import gulp from 'gulp'
-// import startServer from './server.js'
+const fse = require('fs-extra');
+
+function existsFile(dist = 'dist', callback) {
+  fse.exists(dist, (exists) => {
+    if (exists) {
+      callback && callback()
+    } else {
+      shell.echo(`please run build first`)
+    }
+  })
+}
+
+async function copyAssets() {
+  existsFile('dist', async function () {
+    await fse.emptyDir('dist-examples')
+    await gulp
+      .src('./examples/**', { nodir: true })
+      .pipe(gulp.dest('dist-examples'))
+    await gulp
+      .src('./dist/**', { nodir: true })
+      .pipe(gulp.dest('dist-examples/libs/sdk/'))
+  })
+}
 
 gulp.task('server', gulp.series((done) => {
   startServer()
+  done();
+}));
+
+gulp.task('build', gulp.series((done) => {
+  copyAssets()
   done();
 }));
